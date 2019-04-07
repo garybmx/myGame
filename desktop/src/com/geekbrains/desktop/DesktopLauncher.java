535258f5308070b555236a788e0680f5cac21b63
Igor U <garymtb@yandex.ru>
2019-04-08 00:16:32 +0300

    Initial commit

---
+++ desktop/src/com/geekbrains/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.geekbrains.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.geekbrains.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("user.name","\\xD0\\x9A\\xD1\\x80\\xD1\\x8B\\xD0\\xBC");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
