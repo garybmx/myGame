fa15caa11b6e4e717480c4b2a2110262f43f77e7
Igor U <garymtb@yandex.ru>
2019-04-11 17:41:35 +0300

    Урок 2

--- desktop/src/com/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/com/geekbrains/desktop/DesktopLauncher.java
@@ -3,11 +3,12 @@ package com.geekbrains.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.geekbrains.MyGdxGame;
+import com.geekbrains.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		System.setProperty("user.name","\\xD0\\x9A\\xD1\\x80\\xD1\\x8B\\xD0\\xBC");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new StarGame(), config);
 	}
 }
